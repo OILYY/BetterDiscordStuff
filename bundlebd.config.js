1c37824479b6f77057aaff7fa4b8b93c5b43e57d
OILYY <124994484+OILYY@users.noreply.github.com>
2023-10-31 22:33:39 -0600

    Delete bundlebd.config.js

--- bundlebd.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("bundlebd");
-
-module.exports = defineConfig((plugin, dev) => ({
-	input: `${plugin}/src`,
-	output: dev ? `${plugin}/dist` : `${plugin}`,
-	importAliases: {
-		"@lib/*": `Library/src/*`,
-	},
-}));
